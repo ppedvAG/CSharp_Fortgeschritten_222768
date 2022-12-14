0e5ecce9c57625a4b4259a86fd3725a61a49d277
ppedvLukasK <lukask@ppedv.de>
2022-12-14 09:43:10 +0100

    M003

--- DelegatesEvents/M003-LabCode.cs
+++ DelegatesEvents/M003-LabCode.cs
@@ -29,17 +29,25 @@ public class DivisionsCalculator
 	}
 }
 
-// public bool CheckPrime(int num)
-// {
-	// if (num % 2 == 0)
-		// return false;
-	
-	// for (int i = 3; i <= num / 2; i += 2)
-	// {
-		// if (num % i == 0)
-		// {
-			// return false;
-		// }
-	// }
-	// return true;
-// }
+/// <summary>
+/// Überprüft ob eine gegebene Zahl eine Primzahl ist und gibt true oder false zurück.
+/// </summary>
+/// <param name="num">Die Zahl die überprüft werden soll.</param>
+/// <returns>Ist eine Primzahl, oder nicht.</returns>
+//public bool CheckPrime(int num)
+//{
+//	if (num % 2 == 0)
+//	{
+//		return false;
+//	}
+//
+//	for (int i = 3; i <= num / 2; i += 2)
+//	{
+//		if (num % i == 0)
+//		{
+//			return false;
+//		}
+//	}
+//
+//	return true;
+//}
